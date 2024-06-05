af5c2063d986495cc926ec4b201f003fe71bb553
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-06-05 09:40:49 +0200

    expo-router

--- ClickCollect/App.js
+++ ClickCollect/App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bienvenue sur l'App de Nikolaï</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +20,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: '26',
+    textDecorationLine: 'underline',
+  }
 });
+
+
+  
+
